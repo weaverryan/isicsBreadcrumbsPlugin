fac44bc65536ca82f42989999904d096391587cf
Ryan Weaver <ryan@thatsquality.com>
2010-08-29 20:23:02 -0500

    Breaking the breadcrumb classes into two files

--- lib/isicsBreadcrumbs.class.php
+++ lib/isicsBreadcrumbs.class.php
@@ -7,50 +7,6 @@
  * file that was distributed with this source code.
  */
 
-class isicsBreadcrumbsItem
-{
-  protected $text;
-  protected $uri;
-  protected $options;
-    
-  /**
-   * Constructor
-   *
-   */    
-  public function __construct($text, $uri = null, $options = array())
-  {
-    $this->text = $text;
-    $this->uri = $uri;
-    $this->options = $options;
-  }
-  
-  /**
-   * Retrieve the uri of the item
-   *
-   */  
-  public function getUri()
-  {
-    return $this->uri;
-  }
-  
-  /**
-   * Retrieve the text of the item
-   *
-   */  
-  public function getText()
-  {
-    return $this->text;
-  }
-  
-  /**
-   * Retrieve the options of the item
-   *
-   */  
-  public function getOptions()
-  {
-    return $this->options;
-  }
-}
 
 class isicsBreadcrumbs
 {
